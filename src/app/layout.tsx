26f6c38618fa2c809edffd6cf0c90c79dc683829
Mardev <rozakadm@gmail.com>
2024-08-06 17:46:46 +0700

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={poppins.className}>
         <ThemeProvider
         attribute="class"
-        defaultTheme="dark"
+        defaultTheme="light"
         enableSystem
         >
           <Navigation/>
