466419d9dd36dd820803c6a2e17afcc766b05a99
TZ <rozakadm@gmail.com>
2025-11-26 22:29:19 +0700

    FIX

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -27,7 +27,7 @@ const AboutPage = (props: Props) => {
     visible: {
       y: 0,
       opacity: 1,
-      transition: { type: "spring", stiffness: 100 }
+      transition: { type: "spring" as const, stiffness: 100 }
     }
   };
 
